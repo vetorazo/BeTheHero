b433d2c498728f7bc2c162f16982a890c8e40f89
Vetorazo <vetorazo@gmail.com>
2020-03-28 17:53:26 +0900

    Some Validation and Tests made

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const routes = require('./routes');
 
+const { errors } = require('celebrate');
+
 const cors = require('cors');
 
 const app = express();
@@ -10,6 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /**
  * HTTP METHOS
@@ -35,4 +38,4 @@ app.use(routes);
 
 
  
-app.listen(3333);
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//celebrate to use validation
+const { celebrate, Segments, Joi} = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -9,17 +11,44 @@ const routes = express.Router();
 
 //route to login
 routes.post('/sessions', SessionController.create);
+
 //route to get ong names
 routes.get('/ongs', OngController.index);
+
 //route to create a ONG (create account)
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
+
 //route to get profile info
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}),ProfileController.index);
+
 //route to get a incidents
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index);
+
 //route to create a new incident
 routes.post('/incidents', IncidentController.create);
+
 //route to delete a incident
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete)
+
 
 module.exports = routes;
